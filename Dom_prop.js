13ba278d6982efe8eafcf6420215095611e30ac5
SvdNeo <sanjay.d@neoito.com>
2024-01-03 18:20:33 +0530

    Select all diagonal cells

--- Dom_prop.js
+++ Dom_prop.js
@@ -1,8 +1,13 @@
-let newDiv = document.createElement("div");
-newDiv.id = "dynamicDiv";
-newDiv.innerHTML = "<h1>I work for NEOITO</h1>"
-document.body.appendChild(newDiv);
-// var script = document.createElement("script");
-// script.src = "Dom_prop.js"
-// document.body.appendChild(script)
-document.body.appendChild(document.createElement('script')).src = 'Dom_prop.js'
+// let newDiv = document.createElement("div");
+// newDiv.id = "dynamicDiv";
+// newDiv.innerHTML = "<h1>I work for NEOITO</h1>"
+// document.body.appendChild(newDiv);
+// // var script = document.createElement("script");
+// // script.src = "Dom_prop.js"
+// // document.body.appendChild(script)
+// document.body.appendChild(document.createElement('script')).src = 'Dom_prop.js';
+
+//task 1 
+console.log(document.body.firstElementChild)
+console.log(document.body.children[1])
+console.log(document.body.lastElementChild.previousElementSibling.lastElementChild.innerHTML)
